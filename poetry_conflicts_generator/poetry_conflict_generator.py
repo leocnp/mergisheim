0eeb8690263a58c24349d35b6dc9901775a6c84a
leoecrepont <leo.ecrepont@mergify.com>
2023-09-26 16:57:50 +0200

    add conflict generator

---
+++ poetry_conflicts_generator/poetry_conflict_generator.py
@@ -0,0 +1,50 @@
+"""
+Script to generate poetry conflicts to be resolved
+"""
+
+import uuid
+import github
+import subprocess
+
+from poetry_conflicts_generator import utils
+
+
+REPOSITORY_NAME = "mergisheim"
+
+
+gh_client = utils.get_github_client()
+
+
+def reset_packages():
+    """Call before start to recreate the initial scenario"""
+    # TODO
+    pass
+
+
+def main():
+    # 1. reset state of packages to recreate scneario
+    # 2. create the first branch
+
+    reset_packages()
+
+    user = gh_client.get_user()
+    repo = gh_client.get_repo(f"{user.login}/{REPOSITORY_NAME}")
+
+    uid = str(uuid.uuid4()).split("-")[0]
+
+    # Create base PR
+    p0_head_branch = f"{uuid}-p0-base"
+    p0 = repo.create_pull(title=f"PR0({uid}): base", body="", base="main", head=p0_head_branch)
+
+    # Create PR1 with first dependency updated
+    p1 = repo.create_pull(title=f"PR1({uid}): update dependency ", body="", base=p0_head_branch, head=f"{uuid}-p1")
+
+    # Create PR2 with first dependency updated
+    # p2 = repo.create_pull(title=f"PR2({uid}): update dependency ", body="", base=p0_head_branch, head=f"{uuid}-p1")
+
+    # Checkout PR1 and push an update
+
+
+if __name__ == "__main__":
+    # main()
+    utils.create_local_branch("testbranch")
---
+++ poetry_conflicts_generator/utils.py
@@ -0,0 +1,39 @@
+import subprocess
+
+import github
+
+
+CREATED_BRANCHES = []
+
+def created_branch():
+    pass
+
+
+def get_github_client() -> github.Github:
+    token = subprocess.run(["gh", "auth", "token"], capture_output=True)
+    auth = github.Auth.Token(token.stdout.decode().strip())
+    return github.Github(auth=auth)
+
+
+def create_branch(repo, branch_name: str):
+    # TODO?
+    main = repo.get_branch(branch="main")
+    base_commit = repo.get_commits(sha=main.commit.sha)[0]
+
+    # branch = f"{base_branch_name}-{str(uuid.uuid4())}"
+    repo.create_git_ref(f"refs/heads/{branch_name}", base_commit.sha)  # create branch
+    return branch_name
+
+
+def _create_local_branch(name: str):
+    subprocess.run(["git", "checkout", "-b", name])
+
+
+def push_dependencies_update_branch(head: str, package: str, version: str):
+    _create_local_branch(head)
+    print(f"- Create head branch {head} with poetry package {package}={version}")
+    subprocess.run(["git", "push", ""])
+
+
+def create_pull(repo, body: str, base: str, head: str):
+    pass
